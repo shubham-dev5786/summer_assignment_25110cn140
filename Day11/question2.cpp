60cd9ff4b29d9178b8492f969f6139ca78c28039
shubham-dev5786 <svermavivo@gmail.com>
2026-06-12 21:34:10 +0530

    day 11 added

---
+++ Day11/question2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int maximum(int a, int b) {
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    cout << "Maximum = " << maximum(num1, num2);
+
+    return 0;
+}
