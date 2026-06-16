38267edcfe67b92c493f8ec4deea7e40d3387e3a
shubham-dev5786 <svermavivo@gmail.com>
2026-06-16 23:16:14 +0530

    day 15 added

---
+++ Day15/question4.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int j = 0; 
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != 0) {
+            arr[j] = arr[i];
+            j++;
+        }
+    }
+
+    while (j < n) {
+        arr[j] = 0;
+        j++;
+    }
+
+    cout << "Array after moving zeroes to the end: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
